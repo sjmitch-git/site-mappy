175a002dc3bb043fcd75d75c325913bdada4ca53
sjmitch-git <sjmitch20@outlook.com>
2024-05-18 11:29:54 +0100

    Adds logo

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
+
 import Container from './container'
+import logo from '../../public/logo.png'
 
 export default function Header() {
 	return (
 		<header>
-			<Container>#header</Container>
+			<Container className='flex justify-center p-4'>
+				<Image
+					src={logo}
+					alt='Site Logo'
+					width='898'
+					height='133'
+					priority
+				/>
+			</Container>
 		</header>
 	)
 }
