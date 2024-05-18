3755aa828a9bd0d4f8352881aa207189d0b76379
sjmitch-git <sjmitch20@outlook.com>
2024-05-18 08:58:37 +0100

    Adds GA tags

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,18 @@
 const APP_TITLE: string = 'Site Mappy'
-const APP_DESCRIPTION: string = 'Generate Sitemap.xml from a given URL'
+const APP_DESCRIPTION: string = 'Generate a sitemap.xml from a given URL'
 const APP_BASE_URL: string = 'https://site-mappy.vercel.app'
 const APP_AUTHOR: string = 'Stephen Mitchell'
 const APP_AUTHOR_URL: string = 'https://mitchblog.vercel.app/'
+
+const GA_TAG = 'G-KQZNCHD7D0'
+const GOOGLE_VERIFICATION = 'Ws4t6yaR0RsGkflU774SwgOj3duRsycUnDwyXY3SSD0'
+
+export {
+	APP_TITLE,
+	APP_DESCRIPTION,
+	APP_BASE_URL,
+	APP_AUTHOR,
+	APP_AUTHOR_URL,
+	GA_TAG,
+	GOOGLE_VERIFICATION,
+}
