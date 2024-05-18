fa4f9e6bc409926ab87045ddce48e733455aa38e
sjmitch-git <sjmitch20@outlook.com>
2024-05-18 08:36:20 +0100

    Tailwind set-up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--primary-color)",
-        secondary: "var(--secondary-color)",
-        info: "var(--info-color)",
-        success: "var(--success-color)",
-        warning: "var(--warning-color)",
-        danger: "var(--danger-error)",
-        light: "var(--light-color)",
-        dark: "var(--dark-color)",
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/forms")({
-      strategy: "class",
-    }),
-  ],
-};
-export default config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: 'var(--primary-color)',
+				secondary: 'var(--secondary-color)',
+				accent: 'var(--accent-color)',
+				info: 'var(--info-color)',
+				success: 'var(--success-color)',
+				warning: 'var(--warning-color)',
+				danger: 'var(--danger-error)',
+				light: 'var(--light-color)',
+				dark: 'var(--dark-color)',
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms')({
+			strategy: 'class',
+		}),
+	],
+}
+export default config
