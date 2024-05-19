70e4bb320c00315c880dab5fc538095aef6ffffd
sjmitch-git <sjmitch20@outlook.com>
2024-05-19 20:12:09 +0100

    Fixes spinner size

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,5 +1,5 @@
 import { FaSpinner } from 'react-icons/fa'
 
 export default function Spinner() {
-	return <FaSpinner className='animate-spin text-4xl text-light' />
+	return <FaSpinner className='animate-spin text-light' />
 }
